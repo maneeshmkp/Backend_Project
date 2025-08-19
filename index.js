d48b9806c47b1baf2a61edfce84ed2396e4f45cf
Maneesh Kumar <maneeshprajapati2004@gmail.com>
2025-08-19 18:44:44 +0530

    github data added

--- index.js
+++ index.js
@@ -3,6 +3,44 @@ const express = require('express')
 const app = express()
 const port = 4000
 
+const gitHubData = {
+    "squadName": "Super hero squad",
+    "homeTown": "Metro City",
+    "formed": 2016,
+    "secretBase": "Super tower",
+    "active": true,
+    "members": [
+        {
+        "name": "Molecule Man",
+        "age": 29,
+        "secretIdentity": "Dan Jukes",
+        "powers": ["Radiation resistance", "Turning tiny", "Radiation blast"]
+        },
+        {
+        "name": "Madame Uppercut",
+        "age": 39,
+        "secretIdentity": "Jane Wilson",
+        "powers": [
+            "Million tonne punch",
+            "Damage resistance",
+            "Superhuman reflexes"
+        ]
+        },
+        {
+        "name": "Eternal Flame",
+        "age": 1000000,
+        "secretIdentity": "Unknown",
+        "powers": [
+            "Immortality",
+            "Heat Immunity",
+            "Inferno",
+            "Teleportation",
+            "Interdimensional travel"
+        ]
+        }
+    ]
+}
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
@@ -17,6 +55,11 @@ app.get('/login', (req, res) =>{
 
 app.get('/youtube', (req, res) =>{
     res.send('login youtube')
+})
+
+
+app.get('/github', (req, res) =>{
+    res.json(gitHubData)
 })
 
 app.listen(process.env.PORT, () => {
